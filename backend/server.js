60f47b114c87290ff1c84237b922c06fb67ba994
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-07-17 21:06:15 +0200

    Create app template

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,26 @@
-const path = require("path")
-const express = require("express");
+const path = require('path');
+const express = require('express');
+const bp = require('body-parser');
+const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const app = express();
+// Middleware
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
 
 
 // Have Node serve the files for built React app
 app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
 // Handle GET requests to /api route
-app.get("/api", (req, res) => {
-    res.json({ message: "Test message" });
+app.get('/api', (req, res) => {
+    res.json({ message: 'Test message' });
+});
+
+// Handle POST requests to /api/data route
+app.post('/api/data', (req, res) => {
+    res.json({ receivedData: req.body.postedData });
 });
 
 // All other GET requests not handled before will return our React app
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { Component } from 'react';
+import axios from 'axios';
 
+class App extends Component {
+    state = {
+        inputValue: '',
+        data: '',
+    };
 
-function App() {
-  const [data, setData] = React.useState(null);
+    handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await axios.post('/api/data', { postedData: this.state.inputValue });
+        console.log(response.data);
+        this.setState({ data: response.data.receivedData })
+    };
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
-  return (
-    <div>{data}</div>
-  );
-}
+    render() {
+        return (
+            <>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        value={this.state.post}
+                        onChange={e => this.setState({ inputValue: e.target.value })}
+                    />
+                    <button type="submit">submit</button>
+                </form>
+            </>
+        );
+    };
+};
 
 export default App;
