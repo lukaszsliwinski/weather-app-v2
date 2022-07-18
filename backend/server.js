5f6ce9269557bbad44042092529f39c1878dfe84
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-07-18 21:53:13 +0200

    Get weather conditions from openweathermap using axios and render on main page

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
+// Required dependencies
 const path = require('path');
 const express = require('express');
 const bp = require('body-parser');
 const app = express();
+const axios = require('axios');
 
+// Dotenv package
+require('dotenv').config();
+
+// Port number
 const PORT = process.env.PORT || 3001;
+
+// Openweathermap api key
+const apiKey = `${process.env.API_KEY}`;
 
 // Middleware
 app.use(bp.json());
@@ -13,22 +22,107 @@ app.use(bp.urlencoded({ extended: true }));
 // Have Node serve the files for built React app
 app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
-// Handle GET requests to /api route
-app.get('/api', (req, res) => {
-    res.json({ message: 'Test message' });
-});
-
 // Handle POST requests to /api/data route
 app.post('/api/data', (req, res) => {
-    res.json({ receivedData: req.body.postedData });
+
+    // Get city name from request body
+    const city = req.body.city;
+
+    // Create url for chosen city
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+    axios.get(url)
+        .then(response => {
+            let weather = response.data;
+            // post weather parameters
+            res.json({ 
+                place: `${weather.name}, ${weather.sys.country}`,
+                description: `${weather.weather[0].description}`,
+                icon: `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`,
+                temp: `${Math.round(weather.main.temp)}`,
+                sensed: `${Math.round(weather.main.feels_like)}`,
+                min: `${Math.round(weather.main.temp_min)}`,
+                max: `${Math.round(weather.main.temp_max)}`,
+                sunrise: `${formatTime(weather.sys.sunrise + weather.timezone)}`,
+                sunset: `${formatTime(weather.sys.sunset + weather.timezone)}`,
+                wind: `${Math.round(weather.wind.speed * 10) / 10}`,
+                cloudiness: `${weather.clouds.all}`,
+                pressure: `${Math.round(weather.main.pressure)}`,
+                humidity: `${weather.main.humidity}`
+            });
+        })
+        .catch(error => console.log(error))
+
+    // Format date to 'DD-MM-YYYY'
+    const formatTime = function(unixTimestamp) {
+        let date = new Date(unixTimestamp * 1000);
+        let hours = date.getHours();
+        let minutes = '0' + date.getMinutes();
+        return hours + ':' + minutes.slice(-2);
+    };
 });
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+// });
   
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
+
+
+
+/*
+
+if (weather.main == undefined) {
+    // Render if city doesn't exist in db
+    res.json({
+        weather: null,
+        place: `\xa0\xa0${req.body.city} doesn't exist`,
+    });
+} else {
+    // Format date to 'DD-MM-YYYY'
+    const formatTime = function(unixTimestamp) {
+        let date = new Date(unixTimestamp * 1000);
+        let hours = date.getHours();
+        let minutes = '0' + date.getMinutes();
+        return hours + ':' + minutes.slice(-2);
+    }
+
+    // Create weather parameters
+    let place = `${weather.name}, ${weather.sys.country}`,
+        description = `${weather.weather[0].description}`,
+        icon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`,
+        temp = `${Math.round(weather.main.temp)}`,
+        sensed = `${Math.round(weather.main.feels_like)}`,
+        min = `${Math.round(weather.main.temp_min)}`,
+        max = `${Math.round(weather.main.temp_max)}`,
+        sunrise = `${formatTime(weather.sys.sunrise + weather.timezone)}`,
+        sunset = `${formatTime(weather.sys.sunset + weather.timezone)}`,
+        wind = `${Math.round(weather.wind.speed * 10) / 10}`,
+        cloudiness = `${weather.clouds.all}`,
+        pressure = `${Math.round(weather.main.pressure)}`,
+        humidity = `${weather.main.humidity}`;
+
+    // Render view with weather parameters
+    res.json({
+        weather: weather,
+        place: place,
+        description: description,
+        icon: icon,
+        temp: temp,
+        sensed: sensed,
+        min: min,
+        max: max,
+        sunrise: sunrise,
+        sunset: sunset,
+        wind: wind,
+        cloudiness: cloudiness,
+        pressure: pressure,
+        humidity: humidity,
+    });
+};
+
+*/
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios';
 
-class App extends Component {
-    state = {
-        inputValue: '',
-        data: '',
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            city: '',
+            place: '',
+            description: '',
+            icon: '',
+            temp: '',
+            sensed: '',
+            min: '',
+            max: '',
+            sunrise: '',
+            sunset: '',
+            wind: '',
+            cloudiness: '',
+            pressure: '',
+            humidity: '',
+        };
     };
 
     handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await axios.post('/api/data', { postedData: this.state.inputValue });
-        console.log(response.data);
-        this.setState({ data: response.data.receivedData })
+        const response = await axios.post('/api/data', { city: this.state.city });
+        this.setState({ 
+            place: response.data.place,
+            description: response.data.description,
+            icon: response.data.icon,
+            temp: response.data.temp,
+            sensed: response.data.sensed,
+            min: response.data.min,
+            max: response.data.max,
+            sunrise: response.data.sunrise,
+            sunset: response.data.sunset,
+            wind: response.data.wind,
+            cloudiness: response.data.cloudiness,
+            pressure: response.data.pressure,
+            humidity: response.data.humidity,
+        });
     };
 
     render() {
@@ -21,10 +49,24 @@ class App extends Component {
                     <input
                         type="text"
                         value={this.state.post}
-                        onChange={e => this.setState({ inputValue: e.target.value })}
+                        onChange={e => this.setState({ city: e.target.value })}
                     />
                     <button type="submit">submit</button>
                 </form>
+                
+                <p>{this.state.place}</p>
+                <p>{this.state.description}</p>
+                <img src={this.state.icon} />
+                <p>{this.state.temp}</p>
+                <p>{this.state.sensed}</p>
+                <p>{this.state.min}</p>
+                <p>{this.state.max}</p>
+                <p>{this.state.sunrise}</p>
+                <p>{this.state.sunset}</p>
+                <p>{this.state.wind}</p>
+                <p>{this.state.cloudiness}</p>
+                <p>{this.state.pressure}</p>
+                <p>{this.state.humidity}</p>
             </>
         );
     };
