6e346f2b094599b1b99e32e23d96817e0feeaae8
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-07-19 18:18:33 +0200

    Use nested objects in state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,40 +5,46 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            city: '',
+            input: '',
             place: '',
-            description: '',
-            icon: '',
-            temp: '',
-            sensed: '',
-            min: '',
-            max: '',
-            sunrise: '',
-            sunset: '',
-            wind: '',
-            cloudiness: '',
-            pressure: '',
-            humidity: '',
+            conditions: {
+                description: '',
+                icon: '',
+                temp: '',
+                sensed: '',
+                min: '',
+                max: '',
+                sunrise: '',
+                sunset: '',
+                wind: '',
+                cloudiness: '',
+                pressure: '',
+                humidity: '',
+            },
+            forecast: {
+
+            }
         };
     };
 
     handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await axios.post('/api/data', { city: this.state.city });
-        this.setState({ 
-            place: response.data.place,
-            description: response.data.description,
-            icon: response.data.icon,
-            temp: response.data.temp,
-            sensed: response.data.sensed,
-            min: response.data.min,
-            max: response.data.max,
-            sunrise: response.data.sunrise,
-            sunset: response.data.sunset,
-            wind: response.data.wind,
-            cloudiness: response.data.cloudiness,
-            pressure: response.data.pressure,
-            humidity: response.data.humidity,
+        const response = await axios.post('/api/data', { city: this.state.input });
+        this.setState( (state) => {
+            state.place = response.data.place;
+            state.conditions.description = response.data.description;
+            state.conditions.icon = response.data.icon;
+            state.conditions.temp = response.data.temp;
+            state.conditions.sensed = response.data.sensed;
+            state.conditions.min = response.data.min;
+            state.conditions.max = response.data.max;
+            state.conditions.sunrise = response.data.sunrise;
+            state.conditions.sunset = response.data.sunset;
+            state.conditions.wind = response.data.wind;
+            state.conditions.cloudiness = response.data.cloudiness;
+            state.conditions.pressure = response.data.pressure;
+            state.conditions.humidity = response.data.humidity;
+            return state;
         });
     };
 
@@ -48,25 +54,25 @@ class App extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                     <input
                         type="text"
-                        value={this.state.post}
-                        onChange={e => this.setState({ city: e.target.value })}
+                        value={this.state.input}
+                        onChange={e => this.setState({ input: e.target.value })}
                     />
                     <button type="submit">submit</button>
                 </form>
                 
                 <p>{this.state.place}</p>
-                <p>{this.state.description}</p>
-                <img src={this.state.icon} />
-                <p>{this.state.temp}</p>
-                <p>{this.state.sensed}</p>
-                <p>{this.state.min}</p>
-                <p>{this.state.max}</p>
-                <p>{this.state.sunrise}</p>
-                <p>{this.state.sunset}</p>
-                <p>{this.state.wind}</p>
-                <p>{this.state.cloudiness}</p>
-                <p>{this.state.pressure}</p>
-                <p>{this.state.humidity}</p>
+                <p>{this.state.conditions.description}</p>
+                <img src={this.state.conditions.icon} />
+                <p>{this.state.conditions.temp}</p>
+                <p>{this.state.conditions.sensed}</p>
+                <p>{this.state.conditions.min}</p>
+                <p>{this.state.conditions.max}</p>
+                <p>{this.state.conditions.sunrise}</p>
+                <p>{this.state.conditions.sunset}</p>
+                <p>{this.state.conditions.wind}</p>
+                <p>{this.state.conditions.cloudiness}</p>
+                <p>{this.state.conditions.pressure}</p>
+                <p>{this.state.conditions.humidity}</p>
             </>
         );
     };
