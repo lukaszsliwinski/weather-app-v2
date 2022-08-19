58a1494f07b4f85ac0bd64d300f5d4c81363a540
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-08-19 20:12:19 +0200

    Render search form with react components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,17 @@
 import { useState } from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import FormGroup from 'react-bootstrap/FormGroup';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 import Weather from './components/Weather';
 
 function App() {
@@ -32,18 +44,27 @@ function App() {
     };
 
     return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={query}
-                    onChange={e => setQuery(e.target.value)}
-                />
-                <button type="submit">submit</button>
-            </form>
+        <Container fluid="sm" className="">
+            <Row>
+                <Col md={{ span: 10, offset: 1 }} className="">
+                    <Form onSubmit={handleSubmit}>
+                        <FormGroup role="form" className="mb-3">
+                            <Form.Control
+                                placeholder="Enter a city"
+                                aria-describedby="search-button"
+                                value={query}
+                                onChange={e => setQuery(e.target.value)}
+                            />
+                            <Button variant="" id="search-button">
+                                <FontAwesomeIcon icon={faMagnifyingGlass} />
+                            </Button>
+                        </FormGroup>
+                    </Form>
+                </Col>
+            </Row>
 
             {!isEmpty(data)  && <Weather data={data} />}
-        </>
+        </Container>
     );
 };
 
