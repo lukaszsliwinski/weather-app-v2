c424feb18c2c6fe08f1a46f09a7059211b82d171
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-09-01 21:04:50 +0200

    Style search input

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -6,8 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import InputGroup from 'react-bootstrap/InputGroup';
 import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Button from 'react-bootstrap/Button';
 
 // import font awesome
@@ -34,6 +34,11 @@ function App() {
     const [query, setQuery] = useState('');
     const [data, setData] = useState({});
     const [show, setShow] = useState(false);
+
+    const searchInput = useRef();
+
+    // focus search input on hover
+    const setFocus = (focus) => focus ? searchInput.current.focus() : searchInput.current.blur();
 
     // check if state obj is empty
     const isEmpty = (obj) => {
@@ -63,19 +68,25 @@ function App() {
     return (
         <Container fluid="sm" className="mt-4 border pt-3 px-5">
             <Row>
-                <Col md={{ span: 6, offset: 3 }} className="">
+                <Col md={{ span: 6, offset: 3 }}>
                     <Form onSubmit={handleSubmit}>
-                        <InputGroup role="form" className="mb-3">
-                            <Form.Control
-                                placeholder="Enter a city"
-                                aria-describedby="search-button"
-                                value={query}
-                                onChange={e => setQuery(e.target.value)}
-                            />
-                            <Button variant="" id="search-button">
-                                <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+                        <Form.Group role="form" className="flex-row mb-3 text-secondary">
+                            <FloatingLabel label="Enter a city">
+                                <Form.Control
+                                    ref={searchInput}
+                                    size="sm"
+                                    placeholder="Enter a city"
+                                    value={query}
+                                    onChange={e => setQuery(e.target.value)}
+                                    onMouseEnter={() => setFocus(true)}
+                                    onMouseLeave={() => setFocus(false)}
+                                    className="mb-2 fs-5"
+                                />
+                            </FloatingLabel>
+                            <Button variant="outline-dark" type="submit" className="w-100 fw-bold">
+                                <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" /> Search
                             </Button>
-                        </InputGroup>
+                        </Form.Group>
                     </Form>
                 </Col>
             </Row>
