358ad10b16f2fa99cb2dc4e1190b7a774b572dab
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-09-02 16:35:40 +0200

    Move search input to outer component

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,22 +1,16 @@
-import { useState, useRef } from 'react';
-import axios from 'axios';
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import react components
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Button from 'react-bootstrap/Button';
 
 // import font awesome
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // import components
+import SearchInput from '../SearchInput/SearchInput';
 import AlertModal from '../AlertModal/AlertModal';
 import Main from '../CurrentWeather/Main/Main';
 import WeatherConditions from '../CurrentWeather/WeatherConditions/WeatherConditions';
@@ -35,11 +29,6 @@ function App() {
     const [data, setData] = useState({});
     const [show, setShow] = useState(false);
 
-    const searchInput = useRef();
-
-    // focus search input on hover
-    const setFocus = (focus) => focus ? searchInput.current.focus() : searchInput.current.blur();
-
     // check if state obj is empty
     const isEmpty = (obj) => {
         if (obj && Object.keys(obj).length === 0 && Object.getPrototypeOf(obj) === Object.prototype) {
@@ -49,48 +38,10 @@ function App() {
         }
     };
 
-    // fetch weather data on form submit
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (typeof query !== 'string') {
-            setShow(true);
-        } else {
-            const response = await axios.post('/api/weather', { city: query });
-
-            if (response.data.msg === 'error') {
-                setShow(true);
-            } else {
-                setData(response.data);
-            };
-        };
-    };
 
     return (
         <Container fluid="sm" className="mt-4 border pt-3 px-5">
-            <Row>
-                <Col md={{ span: 6, offset: 3 }}>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group role="form" className="flex-row mb-3 text-secondary">
-                            <FloatingLabel label="Enter a city">
-                                <Form.Control
-                                    ref={searchInput}
-                                    size="sm"
-                                    placeholder="Enter a city"
-                                    value={query}
-                                    onChange={e => setQuery(e.target.value)}
-                                    onMouseEnter={() => setFocus(true)}
-                                    onMouseLeave={() => setFocus(false)}
-                                    required
-                                    className="mb-2 fs-5"
-                                />
-                            </FloatingLabel>
-                            <Button variant="outline-dark" type="submit" className="w-100 fw-bold">
-                                <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" /> Search
-                            </Button>
-                        </Form.Group>
-                    </Form>
-                </Col>
-            </Row>
+            <SearchInput query={query} setQuery={setQuery} setData={setData} setShow={setShow}/>
             {alert && <AlertModal setShow={setShow} show={show} query={query}/>}
             {!isEmpty(data) && <Main data={data} />}
             {!isEmpty(data) && <WeatherConditions data={data}/>}
---
+++ frontend/src/SearchInput/SearchInput.js
@@ -0,0 +1,64 @@
+import { useState, useRef } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import react components
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Button from 'react-bootstrap/Button';
+
+// import font awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// import scss
+import "./SearchInput.scss"; 
+
+
+function SearchInput({ query, setQuery, setData, setShow }) {
+    const searchInput = useRef();
+
+    // focus search input on hover
+    const setFocus = (focus) => focus ? searchInput.current.focus() : searchInput.current.blur();
+
+    // fetch weather data on form submit and handle error
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (typeof query !== 'string') {
+            setShow(true);
+        } else {
+            const response = await axios.post('/api/weather', { city: query });
+            (response.data.msg === 'error') ? setShow(true) : setData(response.data);
+        };
+    };
+
+    return (
+        <Row>
+            <Col md={{ span: 6, offset: 3 }}>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group role="form" className="flex-row mb-3 text-secondary">
+                        <FloatingLabel label="Enter a city">
+                            <Form.Control
+                                ref={searchInput}
+                                size="sm"
+                                placeholder="Enter a city"
+                                value={query}
+                                onChange={e => setQuery(e.target.value)}
+                                onMouseEnter={() => setFocus(true)}
+                                onMouseLeave={() => setFocus(false)}
+                                required
+                                className="mb-2 fs-5"
+                            />
+                        </FloatingLabel>
+                        <Button variant="outline-dark" type="submit" className="w-100 fw-bold">
+                            <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" /> Search
+                        </Button>
+                    </Form.Group>
+                </Form>
+            </Col>
+        </Row>
+    );
+};
+
+export default SearchInput;
