d71e226aca69783b596b8a4b42b6cbf13a7986f0
lukaszliwinski <lukaszsliwinski0@gmail.com>
2023-07-29 15:28:09 +0200

    refactor: update folder structure

--- server/index.js
+++ server/index.js
@@ -18,14 +18,14 @@ const NODE_ENV = process.env.NODE_ENV;
 // middleware
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
-app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // post endpoint
 app.post('/api/weather', weather);
 
 // render react app
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 // run server
